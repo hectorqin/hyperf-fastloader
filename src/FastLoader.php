f344fc98ab30cbb6b3e804080613d03f3cb5877e
hector <haitao.qin@dsmake.com>
2020-06-23 09:54:30 +0800

    Fix namespace issue

--- src/FastLoader.php
+++ src/FastLoader.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
-namespace Hyperf\FastLoader;
+namespace Hector\FastLoader;
 
 use Hyperf\Di\ClassLoader as BaseClassLoader;
 use Composer\Autoload\ClassLoader as ComposerClassLoader;
